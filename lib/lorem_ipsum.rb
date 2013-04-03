d7b2bdadf90f61ea9467eac4be1d8684d04423f7
Richard Simms <rgsimms87@gmail.com>
2013-04-03 14:36:01 +1100

    First commit

---
+++ lib/lorem_ipsum.rb
@@ -0,0 +1,6 @@
+class Lorem
+  def self.para (number)
+    text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec consequat, tellus id vehicula tempus, eros turpis molestie justo, id posuere est risus n    ec ante. Maecenas risus libero, placerat eu ultrices a, varius ut dolor. In hac habitasse platea dictumst. Quisque laoreet mauris ut tellus placerat eget commodo felis mattis.     Nullam non lacus metus, id posuere urna. Quisque facilisis lacinia lorem non placerat. Etiam eget urna ipsum, eget venenatis est. Proin lacinia varius facilisis. Vivamus tris    tique lacinia ultricies."
+    number.times { puts text }
+  end
+end
---
+++ lorem_ipsum.gemspec
@@ -0,0 +1,12 @@
+Gem::Specification.new do |s|
+  s.name        = 'lorem_ipsum'
+  s.version     = '0.0.0'
+  s.date        = '2013-04-03'
+  s.summary     = "A dummy text gem."
+  s.description = "A simple gem that generates paragraphs of lorem text."
+  s.authors     = ["Richard Simms"]
+  s.email       = 'rgsimm87@gmail.com'
+  s.files       = ["lib/lorem_ipsum.rb"]
+  s.homepage    =
+    ''
+end
